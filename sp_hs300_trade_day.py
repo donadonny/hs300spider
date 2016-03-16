c83e52e806d33ad9ea6161b9d0333957b270fa37
Roar <myroar@qq.com>
2016-03-16 08:14:43 +0800

    .

--- sp_hs300_trade_day.py
+++ sp_hs300_trade_day.py
@@ -1,4 +1,4 @@
-﻿#coding : utf-8
+#coding : utf-8
 from util import *
 import demjson,datetime 
 
@@ -22,11 +22,8 @@ def is_trade_day(date):
     data2 = http_spider(url_template2%date)
     return len(data)>=1200 or len(data2)>=1200
 
-<<<<<<< HEAD
-def spider_trade_day(range=360):
-=======
+
 def spider_trade_day(range=300):
->>>>>>> 676a11505ee8be241cd3a4f0fecb92980958079f
     today = datetime.datetime.now()
     oneday = datetime.timedelta(days=1)
     today += oneday
